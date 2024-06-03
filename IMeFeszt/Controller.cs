dfafb90d6e34e2c21dd105dc0690ac5e752971dd
Benko Barna <barna@hyperplane.hu>
2024-06-03 10:50:00 +0200

    added sum endpoint

--- IMeFeszt/Controller.cs
+++ IMeFeszt/Controller.cs
@@ -8,9 +8,16 @@ namespace IMeFeszt;
 public class Controller(AppDbContext dbContext) : ControllerBase
 {
     [HttpPut("{name}/{email}/{message}/{amount:int}")]
-    public async Task AddNewDonater(string name, string email, string message, int amount)
+    public async Task<int> AddNewDonater(string name, string email, string message, int amount)
     {
         dbContext.Donaters.Add(new Donater {Name = name, Email = email, Message = message, Amount = amount});
         await dbContext.SaveChangesAsync();
+        return dbContext.Donaters.Sum(d=>d.Amount);
+    }
+    
+    [HttpGet]
+    public async Task<int> GetDonatedAmount()
+    {
+        return dbContext.Donaters.Sum(d=>d.Amount);
     }
 }
